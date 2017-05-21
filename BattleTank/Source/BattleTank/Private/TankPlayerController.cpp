2b1b2bf32b967837d389c5630076276aa0ec1854
Harp1e <github@bplayer.waitrose.com>
2017-05-21 13:19:18 +0100

    V178: OnComponentHit event

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -22,7 +22,7 @@ void ATankPlayerController::Tick(float DeltaTime)
 
 void ATankPlayerController::AimTowardsCrosshair()
 {
-	if (!GetPawn()) { return; }	// e.g. if not possessing
+	if (!GetPawn()) { return; }	// e.g. if not possessing the player
 	auto AimingComponent = GetPawn()->FindComponentByClass<UTankAimingComponent>();
 	if (!ensure(AimingComponent)) { return; }
 
--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,6 +9,12 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 //	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
@@ -31,3 +37,9 @@ void UTankTrack::SetThrottle(float Throttle)
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation, NAME_None);
 }
 
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, 
+		AActor* OtherActor, UPrimitiveComponent* OtherComponent, 
+		FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Track hit"))
+}
